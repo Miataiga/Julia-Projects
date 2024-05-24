1ec11d0a76ae9d4128e1618e3837d6eac17c4d5b
Miataiga <wendyjobert@hotmail.com>
2024-05-24 17:48:20 -0300

    Adds comments
    Adds some comments explaining why I wrote each program.

--- julia_projects/hello_world/hello_world.jl
+++ julia_projects/hello_world/hello_world.jl
@@ -1,3 +1,12 @@
+#=
+    I decided to write hello world in this way because this is the last thing
+    I remember coding. I was trying to learn data science online a while ago 
+    but decided to go to college instead for a more hands-on learning
+    experience. That time I was using Python, but I didn't wanna get ahead
+    of myself for the languages I'd be learning in college so I chose to learn
+    Julia in the meantime!
+=#
+
 greeting = "haiku.txt"
 
 open(greeting,"r") do f
--- julia_projects/squares_with_addition.jl
+++ julia_projects/squares_with_addition.jl
@@ -1,3 +1,16 @@
+#=
+    This is an algorthim I made in grade ten or nine in math
+    class. I was very bad at multiplication and did not want 
+    to memorize all the square numbers up to 100. It was very 
+    fun and I learned how to do this in my head up to 1024.
+    Ironically, I thought about this algorthim so much that 
+    I did end up memorizing all the square numbers up to 100 
+    for the duration of that class, which was the amount we 
+    were meant to know.
+
+    Now it exists in machine form, very cool!
+=#
+
 function squares_with_addition(sqr_num)
     acc = 1
     curr_sqr = 1
